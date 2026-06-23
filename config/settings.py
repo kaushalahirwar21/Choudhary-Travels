0c56583384fa7491f5a0257974693d479683f1f5
KAUSHAL SINGH AHIRWAR <kaushalahirwar714@gmail.com>
2026-06-24 01:18:34 +0530

    Allow all hosts and reconfigure for new database

--- config/settings.py
+++ config/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY', default='django-insecure-dev-key-change
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DJANGO_DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
