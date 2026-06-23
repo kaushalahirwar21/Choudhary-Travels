b44d794dc42048b86d4143b43445d8f773574b63
KAUSHAL SINGH AHIRWAR <kaushalahirwar714@gmail.com>
2026-06-23 23:44:54 +0530

    Fix merge conflict in build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
 
 # Exit on error
 set -o errexit
 
-# Poetry configuration
-# python -m pip install poetry
-# python -m poetry config virtualenvs.in-project true
-# python -m poetry install --no-root
-
-# Pip configuration
+# Install dependencies
 pip install -r requirements.txt
 
 # Collect static files
@@ -17,11 +11,3 @@ python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py migrate
-=======
-set -o errexit
-
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
-python manage.py ensure_superuser
->>>>>>> 85581e2fb793ac61c7fcc6a98dec5ac5ab2ee5b8
