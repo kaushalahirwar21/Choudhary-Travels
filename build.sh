c004958773a1cea9816bb08d6b1d6d6d14adcc53
KAUSHAL SINGH AHIRWAR <kaushalahirwar714@gmail.com>
2026-06-24 10:39:47 +0530

    Configure for Vercel deployment and clean up project

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "BUILD SCRIPT STARTED: Attempting to run migrations..."
-
-python manage.py migrate --no-input
-
-echo "BUILD SCRIPT FINISHED: Migrations complete."
---
+++ build_files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run migrations
+python manage.py migrate
