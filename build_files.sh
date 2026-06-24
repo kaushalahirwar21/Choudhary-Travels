444144463bacaf0f326e1252cf19f018f10b74b0
KAUSHAL SINGH AHIRWAR <kaushalahirwar714@gmail.com>
2026-06-24 22:09:03 +0530

    Set temporary superuser password to 1234

--- build_files.sh
+++ build_files.sh
@@ -4,6 +4,8 @@ python3 -m pip install -r requirements.txt --break-system-packages
 python3 manage.py collectstatic --noinput --clear
 python3 manage.py migrate --noinput
 python3 manage.py ensure_superuser
+python3 -c "import django; django.setup(); from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', '1234')" || true
 echo "BUILD END"
+
 
 
