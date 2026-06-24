19b92b390d594a5c2342467c0e822ab419aa5b84
KAUSHAL SINGH AHIRWAR <kaushalahirwar714@gmail.com>
2026-06-24 22:05:51 +0530

    Run ensure_superuser management command during Vercel build

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,7 @@ echo "BUILD START"
 python3 -m pip install -r requirements.txt --break-system-packages
 python3 manage.py collectstatic --noinput --clear
 python3 manage.py migrate --noinput
+python3 manage.py ensure_superuser
 echo "BUILD END"
+
 
