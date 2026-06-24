c85cb836d18467f0275afc797fb99ec5ad1b97ad
KAUSHAL SINGH AHIRWAR <kaushalahirwar714@gmail.com>
2026-06-24 21:57:00 +0530

    Add automatic database migration to build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -2,4 +2,6 @@
 echo "BUILD START"
 python3 -m pip install -r requirements.txt --break-system-packages
 python3 manage.py collectstatic --noinput --clear
+python3 manage.py migrate --noinput
 echo "BUILD END"
+
