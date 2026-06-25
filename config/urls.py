2e0147e8a2f7dc1865c0effc2b18bfe613f922c8
KAUSHAL SINGH AHIRWAR <kaushalahirwar714@gmail.com>
2026-06-25 13:01:53 +0530

    Implement SEO Optimization Package: custom meta descriptions, robots.txt, sitemap.xml, and local business JSON-LD schema

--- config/urls.py
+++ config/urls.py
@@ -5,12 +5,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
 from booking import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('secure-login/', RedirectView.as_view(url='/admin/', permanent=False)),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml', content_type='application/xml')),
     path('', include('booking.urls')),
 ]
 
